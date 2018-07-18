60fe78e039b69f868a2390fa10d386d272a04734
gtryus <gtrihus59@yahoo.com>
2018-07-18 14:02:06 -0500

    Installer (#2)
    * First draft of installer
    * Update installer so desk icon works
    * Icon and branding to be added.

--- src/portable/Transcribe.Windows/Properties/AssemblyInfo.cs
+++ src/portable/Transcribe.Windows/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Transcribe.Windows")]
 [assembly: AssemblyDescription("Transcribe minority languages for review by Paratext")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("Transcriber")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,15 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("259c1ae4-7715-4c7a-8ed1-2f0c71e90b0d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
