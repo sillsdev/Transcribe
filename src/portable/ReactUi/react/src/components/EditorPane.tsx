d9a81aa78bbe97715afc4caa311ca04c8eee7723
gtryus <gtryus@gmail.com>
2018-07-31 14:50:57 -0500

    Update edit panel to use user settings for font and size

--- src/portable/ReactUi/react/src/components/EditorPane.tsx
+++ src/portable/ReactUi/react/src/components/EditorPane.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { IState } from '../model/state';
 import './EditorPane.css'
 
-class EditorPane extends React.Component<any> {
+class EditorPane extends React.Component<IStateProps, any> {
     public render() {
+        const { users, selectedProject, selectedUser } = this.props;
+        const user = users.filter(u => u.username.id === selectedUser)[0];
+        const project = user && user.project.filter(p => p.id === selectedProject)[0];
+        const font = project != null? project.fontfamily: "SIL Charis"; // Tests null or undefined
+        const size = project != null? project.fontsize: "12pt"; // Tests null or undefined
+
         return (
             <div className="EditorPane"> 
-                <textarea />
+                <textarea style={{fontFamily: font, fontSize: size}} />
             </div>
         )
     }
 };
 
-export default EditorPane;
+interface IStateProps {
+    selectedUser: string;
+    selectedProject: string;
+    users: IUser[];
+};
+
+const mapStateToProps = (state: IState): IStateProps => ({
+    selectedProject: state.tasks.selectedProject,
+    selectedUser: state.users.selectedUser,
+    users: state.users.users,
+});
+
+export default connect(mapStateToProps)(EditorPane);
