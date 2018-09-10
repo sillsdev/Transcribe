8779cbe91e47b3706f86ea1882f66f97a7655cf3
gtryus <gtryus@gmail.com>
2018-09-10 11:58:09 -0500

    Update dependency versions, add logging
    - Add SIL.Core.Desktop for Logger reporting
    - Update to 8.1.6 of ParatextData
    - Update to 2.3.4 of icu
    - Update to 5.0.5 of SIL.Lib
    - Build ReactShared separately and save in lib folder.

--- src/portable/Transcribe.Windows/Program.cs
+++ src/portable/Transcribe.Windows/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Threading;
 using Gecko;
 using ReactShared;
+using SIL.Reporting;
 using Transcribe.Properties;
 
 
@@ -25,6 +26,8 @@ namespace Transcribe.Windows
 			Application.SetCompatibleTextRenderingDefault(false);
 			Xpcom.Initialize("Firefox");
 			Util.DataFolder = Path.GetDirectoryName(Application.CommonAppDataPath);
+			Logger.Init(Path.Combine(Util.DataFolder, Application.ProductVersion));
+			Logger.WriteEvent("Launch {0} {1}", Application.ProductName, Application.ProductVersion);
 			var randomName = Path.GetTempFileName();
 			if (File.Exists(randomName))
 				File.Delete(randomName);
