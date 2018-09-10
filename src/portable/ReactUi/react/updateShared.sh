9e68a2182f64c37ab52b55b0a62d075bf60af0b7
gtryus <gtryus@gmail.com>
2018-09-10 13:19:01 -0500

    Move update shared logic to npm run build
    When npm run build command is used the program builds the typescript,
    puts the build file and data into ReactShared, updates ReactShared.csproj
    builds ReactShared and copies the resulting assembly to the lib folder
    (The dependencies for windows and linux point to the lib folder.)

---
+++ src/portable/ReactUi/react/updateShared.sh
@@ -0,0 +1,20 @@
+﻿#/usr/bash echo Updating ReactShared...
+cd ../../../UpdateSharedResources
+dotnet run ../portable/ReactUi/react/build ../portable/ReactShared/ReactShared.csproj
+cd ../portable/ReactShared
+rm -rf data
+mkdir data
+cp ../ReactUi/data/*.xsd data
+cp ../ReactUi/data/*.xml data
+cp ../ReactUi/data/*.eaf data
+cd data
+mkdir localization
+cp ../../ReactUi/data/localization/*.json localization
+cd ..
+rm -rf react
+mkdir react
+cd react
+mkdir build
+cp -r ../../ReactUi/react/build/* build
+cd ..
+dotnet build
