1034e24b7a4c4aa9c7dc93c32c3b5c1b320c61b3
gtryus <gtryus@gmail.com>
2018-09-12 08:56:54 -0500

    Update ReactShared build script
    - use .bat format since bash won't run on Team City

---
+++ src/portable/ReactUi/react/build.bat
@@ -0,0 +1,20 @@
+call npm run build
+cd ..\..\..\UpdateSharedResources
+dotnet run ..\portable\ReactUi\react\build ..\portable\ReactShared\ReactShared.csproj
+cd ..\portable\ReactShared
+rmdir /s/q data
+mkdir data
+copy ..\ReactUi\data\*.xsd data
+copy ..\ReactUi\data\*.xml data
+copy ..\ReactUi\data\*.eaf data
+cd data
+mkdir localization
+copy ..\..\ReactUi\data\localization\*.json localization
+cd ..
+rmdir /s/q react
+mkdir react
+cd react
+mkdir build
+xcopy /s/y ..\..\ReactUi\react\build\* build
+cd ..
+dotnet build
