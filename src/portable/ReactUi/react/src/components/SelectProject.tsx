59b019fec4263d2c2185fefbe7b012d3996811ad
gtryus <gtryus@gmail.com>
2018-10-12 10:39:14 -0500

    Update logic so only admin users go to /uilang choice

--- src/portable/ReactUi/react/src/components/SelectProject.tsx
+++ src/portable/ReactUi/react/src/components/SelectProject.tsx
@@ -21,9 +21,20 @@ class SelectProject extends React.Component<IProps, object> {
   }
 
   public render() {
-    const { loaded, projects, selectProject } = this.props
+    const { loaded, projects, selectProject, selectedUser, users } = this.props
 
     const dest = "/main";
+
+    const user = users && users.filter(u =>  u.username.id === selectedUser)[0]
+    const admin = user && user.role && user.role.filter(r => r === 'administrator')[0]
+
+    if (loaded){
+      if (admin && projects.length === 0) {
+        return <Redirect to="/uilang"/>
+      } else if (projects.length === 1) {
+        return <Redirect to={dest}/>
+      }
+    }
 
     const avatars = projects.map((p:IProject) => 
       <ListGroupItem key={p.id}>
@@ -34,24 +45,11 @@ class SelectProject extends React.Component<IProps, object> {
           select={selectProject} />
       </ListGroupItem>);
 
-    let wrapper = (<ListGroup>
-      {avatars}
-    </ListGroup>)
-    if (loaded){
-      switch (projects.length){
-        case 0:
-          wrapper = (<Redirect to="/uilang"/>)
-          break;
-        case 1:
-          wrapper = (<Redirect to={dest}/>)
-        default:
-          break;
-      }
-    }
-
     return (
       <div className="SelectProject">
-        {wrapper}
+        <ListGroup>
+          {avatars}
+        </ListGroup>
       </div>
     );
   }
@@ -62,6 +60,7 @@ interface IStateProps {
   projects: IProject[];
   loaded: boolean;
   strings: ITranscriberStrings;
+  users: IUser[];
 };
 
 const mapStateToProps = (state: IState): IStateProps => ({
@@ -69,6 +68,7 @@ const mapStateToProps = (state: IState): IStateProps => ({
   projects: state.tasks.projects,
   selectedUser: state.users.selectedUser,
   strings: state.strings.transcriber,
+  users: state.users.users,
 });
 
 interface IDispatchProps {
