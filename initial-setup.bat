3c04657db06783e63c4fbc8a0dbb984fbb6d09b9
Greg Trihus <greg_trihus@sil.org>
2018-10-20 21:52:35 -0500

    ensure initial-setup works if Firefox folders present

--- initial-setup.bat
+++ initial-setup.bat
@@ -6,7 +6,7 @@ cd ..\..\..\..
 git clone https://github.com/mono/nuget-binary.git lib/nuget
 .\lib\nuget\nuget.exe restore .\Transcribe.Windows.sln
 rem dotnet msbuild /t:Rebuild /p:SolutionDir=.\;Configuration=Release;Platform=x86;OutputPath=.\output\Release
-xcopy /s/i .\packages\Geckofx45.45.0.34\content\Firefox .\output\Debug\Firefox
-xcopy /s/i .\packages\Geckofx45.45.0.34\content\Firefox .\output\Release\Firefox
-xcopy /s/i .\packages\Geckofx45.64.Linux.45.0.37\content\Firefox-Linux64 .\src\portable\Transcribe.Linux\bin\Debug\Firefox
-xcopy /s/i .\packages\Geckofx45.64.Linux.45.0.37\content\Firefox-Linux64 .\src\portable\Transcribe.Linux\bin\Debug\Firefox
+xcopy /s/i/y .\packages\Geckofx45.45.0.34\content\Firefox .\output\Debug\Firefox
+xcopy /s/i/y .\packages\Geckofx45.45.0.34\content\Firefox .\output\Release\Firefox
+xcopy /s/i/y .\packages\Geckofx45.64.Linux.45.0.37\content\Firefox-Linux64 .\src\portable\Transcribe.Linux\bin\Debug\Firefox
+xcopy /s/i/y .\packages\Geckofx45.64.Linux.45.0.37\content\Firefox-Linux64 .\src\portable\Transcribe.Linux\bin\Release\Firefox
