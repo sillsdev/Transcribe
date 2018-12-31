1f2bc62e20a0a84795b68db729b44efbc9a81334
vkarthim <vkarthim@gmail.com>
2018-12-31 18:06:41 +0530

    TT-315: TimeMarker test(s)
    * Added NUnit CountDown and CountUp test case for this TimeMaker control

---
+++ src/portable/ReactUi/react/src/__test__/TimeMarker1.test.js
@@ -0,0 +1,41 @@
+/* jshint esversion: 6 */
+import * as React from 'react';
+import expect from 'expect';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import * as actions from '../actions/audioActions';
+import TimeMarker from '../components/controls/TimeMarker';
+
+configure({ adapter: new Adapter() })
+
+// Snapshot for TimeMarker
+describe('>>>Control: TimeMarker --- Snapshot',()=>{
+    const minProps = {
+        playedSeconds: 10,
+        totalSeconds: 30,
+        timer: "countup",
+    };
+
+    it('+++capturing Snapshot of TimeMarker', () => {
+        const renderedValue =  renderer.create(<TimeMarker {...minProps}/>).toJSON()
+        expect(renderedValue).toMatchSnapshot();
+    });
+});
+//*******************************************************************************************************
+describe('>>>Control: TimeMarker', () => {
+    let wrapper;
+    const minProps = {
+        target: (context) => {return false;},
+        text: "My Filter",
+        type: "safe",
+    };
+
+    beforeEach(()=>{
+        wrapper = shallow(<TimeMarker {...minProps} />)
+    })
+
+    it('+++ renders without exploding', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
