af752c86ef0f17897cffdb4fc0b901d9c61ed640
vkarthim <vkarthim@gmail.com>
2018-12-31 18:37:43 +0530

    TT-316: ToggleSwitch test(s)
    * Added NUnit test case for this ToggleSwitch control

---
+++ src/portable/ReactUi/react/src/__test__/ToggleSwitch.test.js
@@ -0,0 +1,41 @@
+/* jshint esversion: 6 */
+import * as React from 'react';
+import expect from 'expect';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import * as actions from '../actions/audioActions';
+import ToggleSwitch from '../components/controls/ToggleSwitch';
+
+configure({ adapter: new Adapter() })
+
+// Snapshot for ToggleSwitch
+describe('>>>Control: ToggleSwitch --- Snapshot',()=>{
+    const minProps = {
+        text: "ToggleSwitch1",
+        switched: false,
+        enabled: true,
+    };
+
+    it('+++capturing Snapshot of ToggleSwitch', () => {
+        const renderedValue =  renderer.create(<ToggleSwitch {...minProps}/>).toJSON()
+        expect(renderedValue).toMatchSnapshot();
+    });
+});
+//*******************************************************************************************************
+describe('>>>Control: ToggleSwitch', () => {
+    let wrapper;
+    const minProps = {
+        text: "ToggleSwitch1",
+        switched: false,
+        enabled: true,
+    };
+
+    beforeEach(()=>{
+        wrapper = shallow(<ToggleSwitch {...minProps} />)
+    })
+
+    it('+++ renders without exploding', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
